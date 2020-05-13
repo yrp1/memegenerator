62e57582388e1758157cc602d1663917ef5b1c9a
yrp1 <61746292+yrp1@users.noreply.github.com>
2020-05-13 16:27:42 -0400

    Updates

---
+++ precache-manifest.ac2a0e5c039eb4c7f89b2b25714c6a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7d2345a8887f99712bc78684ae1ea6",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "a936a2f749afb98ac52d",
+    "url": "/memegenerator/static/css/main.686456ff.chunk.css"
+  },
+  {
+    "revision": "526581da6af169ed7e44",
+    "url": "/memegenerator/static/js/2.21747658.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegenerator/static/js/2.21747658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a936a2f749afb98ac52d",
+    "url": "/memegenerator/static/js/main.a759c5f9.chunk.js"
+  },
+  {
+    "revision": "792b653942d464745ac0",
+    "url": "/memegenerator/static/js/runtime-main.ea32624c.js"
+  }
+]);
